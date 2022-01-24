909d65fc1db3e94b508457e9c5ccb2d60fe1d16f
Barolex <67413923+Barolex@users.noreply.github.com>
2022-01-24 20:50:44 +0200

    Before addtask.

--- src/Components/MainButton.js
+++ src/Components/MainButton.js
@@ -2,7 +2,11 @@ import Button from "@mui/material/Button";
 
 export const MainButton = ({ text, color, buttonAction }) => {
   return (
-    <Button variant="outlined" color={color} onClick={buttonAction}>
+    <Button
+      variant="outlined"
+      color={color}
+      onClick={buttonAction}
+    >
       {text}
     </Button>
   );
